eba6885f6d054cc9096dad5a810ee2fdfd517436
JMatiasF <124095245+JMatiasF@users.noreply.github.com>
2023-11-07 11:09:59 -0300

    Update script.js
    actualizo js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function suma(a, b, callback) {
+    let c = a + b;
+    callback(c);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const inputA = document.getElementById("inputA");
+    const inputB = document.getElementById("inputB");
+    const sumButton = document.getElementById("sumButton");
+    const result = document.getElementById("result");
+
+    sumButton.addEventListener("click", function () {
+        const numA = parseFloat(inputA.value);
+        const numB = parseFloat(inputB.value);
+
+        suma(numA, numB, function (resultado) {
+            result.textContent = resultado;
+        });
+    });
+});
 
